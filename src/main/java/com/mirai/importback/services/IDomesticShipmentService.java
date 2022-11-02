a66f9388d62a5dd410c034c354238192c28c4237
Edgar <u202020371@upc.edu.pe>
2022-11-02 11:06:28 -0500

    feat(domesticShipment): added service for DomesticShipment component

---
+++ src/main/java/com/mirai/importback/services/IDomesticShipmentService.java
@@ -0,0 +1,6 @@
+package com.mirai.importback.services;
+
+import com.mirai.importback.entities.DomesticShipment;
+
+public interface IDomesticShipmentService extends CrudService<DomesticShipment> {
+}
