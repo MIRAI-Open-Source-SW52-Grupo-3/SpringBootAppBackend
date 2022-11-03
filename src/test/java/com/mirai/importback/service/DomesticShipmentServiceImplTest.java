ad2e3defda3260c06614f2aa5fca81be2b6df1ba
Edgar <u202020371@upc.edu.pe>
2022-11-03 17:49:10 -0500

    feat(domesticShipmentTest): added test for DomesticShipment controller

--- src/test/java/com/mirai/importback/service/DomesticShipmentServiceImplTest.java
+++ src/test/java/com/mirai/importback/service/DomesticShipmentServiceImplTest.java
@@ -61,11 +61,11 @@ public class DomesticShipmentServiceImplTest {
                     "https://img.freepik.com/vector-gratis/dispositivo-telefono-inteligente-realista_52683-29765.jpg?size=338&ext=jpg&ga=GA1.2.459603537.1666060327",
                     "Iphone 13","1","09/09/09","8","09/09/09","olva courier","1289","AWE458",
                     "Chiclayo"));
-            list.add(new DomesticShipment(1L,
+            list.add(new DomesticShipment(2L,
                     "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ9gaYbIm9wrfg43NwHaVCwu7A7mn3HAaj-mw&usqp=CAU",
                     "Armani Perfume","3","09/09/09","12","09/09/09","olva courier","1289","ASD888",
                     "Juliaca"));
-            list.add(new DomesticShipment(1L,
+            list.add(new DomesticShipment(3L,
                     "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQKzFknHBs52FBby5EgUUMnc96vdVt3qL5Ylw&usqp=CAU",
                     "Guess Watch","2","09/09/09","7","09/09/09","olva courier","1289","QQQ965",
                     "Piura"));
